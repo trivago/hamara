214e7314625ac1a7b2898d31032b640704fd2a3c
Ali Ariff <ali.ariff12@gmail.com>
2019-08-23 12:03:00 +0200

    update test

--- cmd/export_test.go
+++ cmd/export_test.go
@@ -1,63 +1,36 @@
 package cmd
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/h2non/gock.v1"
 )
 
 func TestExportCmd(t *testing.T) {
 	tests := []struct {
-		name    string
-		args    []string
-		fixture string
+		name  string
+		args  []string
+		host  string
+		token string
 	}{
-		{"no arguments", []string{}, "export-no-args.golden"},
-		{"valid arguments", []string{"--host=http://localhost:3000", "--token=123"}, "datasources.golden.yaml"},
+		{"no arguments", []string{}, "", ""},
+		{"missing host", []string{"--token=123"}, "", "123"},
+		{"missing token", []string{"--host=http://localhost:3000"}, "http://localhost:3000", ""},
+		{"valid arguments", []string{"--host=http://localhost:3000", "--token=123"}, "http://localhost:3000", "123"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert := assert.New(t)
 
-			defer gock.Off() // Flush pending mocks after test execution
+			actual := new(bytes.Buffer)
+			exportCmd := newExportCmd(actual)
+			exportCmd.SetArgs(tt.args)
+			exportCmd.Execute()
 
-			mockResponse, err := ioutil.ReadFile("testdata/datasources.golden.json")
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			var datasourcesMap []map[string]interface{}
-			json.Unmarshal(mockResponse, &datasourcesMap)
-			gock.New("http://localhost:3000").
-				Get("/api/datasources").
-				Reply(200).
-				JSON(datasourcesMap)
-
-			args := []string{"export"}
-			args = append(args, tt.args...)
-
-			r, w, _ := os.Pipe()
-			os.Stdout = w
-			os.Stderr = w
-
-			rootCmd.SetArgs(args)
-			rootCmd.Execute()
-
-			w.Close()
-			actual, _ := ioutil.ReadAll(r)
-
-			expected, err := ioutil.ReadFile("testdata/" + tt.fixture)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			assert.Equal(actual, expected)
+			assert.Equal(tt.host, exportCmd.Flag("host").Value.String())
+			assert.Equal(tt.token, exportCmd.Flag("token").Value.String())
 		})
 	}
-
 }
